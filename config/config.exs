272d7ef4560382294fc3965ee258aa6f3dc8d5ab
Hornsby, Matt <Matt.L.Hornsby@nordstrom.com>
2018-01-21 16:02:24 -0800

    Initial commit

---
+++ config/config.exs
@@ -0,0 +1,36 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+use Mix.Config
+config :aprs,
+    server: 'first.aprs.net',
+    port: 14580,
+    default_filter: "r/47.6/-122.3/100",
+    login_id: System.get_env("APRS_CALLSIGN"),
+    password: System.get_env("APRS_PASSCODE")
+
+# This configuration is loaded before any dependency and is restricted
+# to this project. If another project depends on this project, this
+# file won't be loaded nor affect the parent project. For this reason,
+# if you want to provide default values for your application for
+# 3rd-party users, it should be done in your "mix.exs" file.
+
+# You can configure your application as:
+#
+#     config :aprs, key: :value
+#
+# and access this configuration in your application as:
+#
+#     Application.get_env(:aprs, :key)
+#
+# You can also configure a 3rd-party app:
+#
+#     config :logger, level: :info
+#
+
+# It is also possible to import configuration files, relative to this
+# directory. For example, you can emulate configuration per environment
+# by uncommenting the line below and defining dev.exs, test.exs and such.
+# Configuration from the imported file will override the ones defined
+# here (which is why it is important to import them last).
+#
+#     import_config "#{Mix.env}.exs"
---
+++ lib/aprs.ex
@@ -0,0 +1,79 @@
+defmodule Aprs do
+  use GenServer
+  require Logger
+
+  def start_link do
+    server = Application.get_env(:aprs, :server, 'rotate.aprs.net')
+    port = Application.get_env(:aprs, :port, 14580)
+    default_filter = Application.get_env(:aprs, :default_filter, "r/47.6/-122.3/100")
+    GenServer.start_link(__MODULE__, [server, port, default_filter], name: __MODULE__)
+  end
+
+  def init([server, port, default_filter]) do
+    aprs_user_id = Application.get_env(:aprs, :login_id, "CHANGE_ME")
+    aprs_passcode = Application.get_env(:aprs, :password, "-1")
+
+    opts = [:binary, active: true]
+    {:ok, socket} = :gen_tcp.connect(server, port, opts)
+    login_string = "user #{aprs_user_id} pass #{aprs_passcode} vers test 1.0 filter #{default_filter} \n"
+    Logger.debug("Using server: #{server}:#{port}")
+    Logger.debug("Logging in with string: #{login_string}")
+    :gen_tcp.send(socket, login_string)
+    {:ok, %{server: server, port: port, socket: socket}}
+  end
+
+  def stop() do
+    Logger.info("Stopping Server")
+    GenServer.stop(__MODULE__, :stop)
+  end
+
+  def set_filter(filter_string), do: send_message("#filter #{filter_string}")
+  def list_active_filters(), do: send_message("#filter?")
+
+  def send_message(from, to, message) do
+    padded_callsign = String.pad_trailing(to, 9)
+    send_message("#{from}>APRS,TCPIP*::#{padded_callsign}:#{message}")
+  end
+
+  def send_message(message) do
+    GenServer.call(__MODULE__, {:send_message, message <> "\r"})
+  end
+
+  def handle_call({:send_message, message}, _from, state) do
+    Logger.debug("Sending message: #{message}")
+    :gen_tcp.send(state.socket, message)
+    {:reply, :ok, state}
+  end
+
+  def handle_info({:tcp, socket, packet}, state) do
+    dispatch(packet)
+    {:noreply, state}
+  end
+
+  def handle_info({:tcp_closed, socket}, state) do
+    Logger.info("Socket has been closed")
+    {:noreply, state}
+  end
+
+  def handle_info({:tcp_error, socket, reason}, state) do
+    IO.inspect(socket, label: "connection closed dut to #{reason}")
+    {:noreply, state}
+  end
+
+  def terminate(reason, state) do
+    # Do Shutdown Stuff
+    Logger.info("Going Down: #{inspect(state)}")
+    :gen_tcp.close(state.socket)
+    :normal
+  end
+
+  defp dispatch("#" <> comment_text) do
+    Logger.debug("COMMENT:" <> String.trim(comment_text))
+  end
+
+  defp dispatch(message) do
+    parsed_message = Parser.parse(message)
+    IO.inspect(parsed_message)
+    #Logger.debug("SERVER:" <> message)
+  end
+end
