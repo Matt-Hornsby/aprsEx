e0ca6d7361880859e997bc337cf5c3c75146d4d0
Hornsby, Matt <Matt.L.Hornsby@nordstrom.com>
2018-01-21 17:49:30 -0800

    Converting to a supervised app with automatic login on startup

---
+++ lib/aprs/application.ex
@@ -0,0 +1,15 @@
+defmodule Aprs.Application do
+    use Application
+  
+    def start(_, _) do
+      import Supervisor.Spec
+  
+      children = [
+        worker(Aprs, []),
+      ]
+  
+      opts = [strategy: :one_for_one, name: Aprs.Supervisor]
+  
+      Supervisor.start_link(children, opts)
+    end
+  end
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Aprs.Mixfile do
     [
       app: :aprs,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -14,7 +14,8 @@ defmodule Aprs.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Aprs.Application, []}
     ]
   end
 
